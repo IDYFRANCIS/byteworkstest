30a1ebd32d332d4e2f3374426a40dd0a72bb48fe
IDONGESIT FRANCIS UKUT <idongesitukut25@gmail.com>
2019-11-02 09:59:35 +0100

    second commit with email module added

--- src/main/java/com/francis/byteworkstest/security/SecurityConfig.java
+++ src/main/java/com/francis/byteworkstest/security/SecurityConfig.java
@@ -75,25 +75,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(SIGNING_KEY);
-        return converter;
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
-
-    @Primary
-    @Bean
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        defaultTokenServices.setSupportRefreshToken(true);
-        return defaultTokenServices;
-    }
-
+    
 }
